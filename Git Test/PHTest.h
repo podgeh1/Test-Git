d7375403b7f168790324bd186bc021ce409bccf8
Podge Hession <Podge@aoncorp-think.gateway.2wire.net>
2014-08-01 11:17:01 -0700

    added a test class

---
+++ Git Test/PHTest.h
@@ -0,0 +1,13 @@
+//
+//  PHTest.h
+//  Git Test
+//
+//  Created by Podge Hession on 01/08/2014.
+//  Copyright (c) 2014 hession. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PHTest : NSObject
+
+@end
---
+++ Git Test/PHTest.m
@@ -0,0 +1,13 @@
+//
+//  PHTest.m
+//  Git Test
+//
+//  Created by Podge Hession on 01/08/2014.
+//  Copyright (c) 2014 hession. All rights reserved.
+//
+
+#import "PHTest.h"
+
+@implementation PHTest
+
+@end
